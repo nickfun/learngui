fb9b373a487be2a3085f562e6c41df9c5093951e
Nick Fun <nick@nick.gs>
2014-07-03 16:17:28 -0700

    pass in a filename on the command line.

--- src/main/java/gs/nick/learngui/Application.java
+++ src/main/java/gs/nick/learngui/Application.java
@@ -11,13 +11,27 @@ package gs.nick.learngui;
  * @author nick fun
  */
 public class Application {
-    
+
     public static void main(String[] args) {
+        Application app = new Application();
+        app.exec(args);
+    }
+    
+    public void exec(String[] args) {
         System.out.println("Hello World");
+        String file = getInputFilename(args);
+        System.out.println("Input file is " + file);
         MainWindow w = new MainWindow();
         w.setTitle("GUI Learning Platform");
         w.clearLists();
         w.setVisible(true);
+    }
+
+    public String getInputFilename(String[] args) {
+        if (args.length == 0) {
+            return "input.json";
+        }
+        return args[0];
     }
     
 }
